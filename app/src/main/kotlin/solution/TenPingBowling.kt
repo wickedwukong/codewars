573a1b64d2ee9d6bab2e5fde984e740f51e27230
Xuemin Guan <gxuemin@gmail.com>
2023-03-03 09:15:52 +0000

    XG: it can be a function instead of a class

--- app/src/main/kotlin/solution/TenPingBowling.kt
+++ app/src/main/kotlin/solution/TenPingBowling.kt
@@ -8,20 +8,18 @@ import solution.Frame.Strike
  * 4 kyu
  * https://www.codewars.com/kata/5531abe4855bcc8d1f00004c/train/java
  */
-class TenPingBowling {
-    fun score(rawFrameInput: String): Int =
-        rawFrameInput
-            .frames()
-            .mapIndexed { index, frame ->
-                fun nextRoll() = rawFrameInput.frames().getOrNull(index + 1)?.firstRollScore ?: 0
-                fun nextNextRoll() = rawFrameInput.frames().getOrNull(index + 1)?.secondRollScore
-                    ?: rawFrameInput.frames().getOrNull(index + 2)?.firstRollScore ?: 0
 
-                when (frame) {
-                    is Strike -> frame.score + nextRoll() + nextNextRoll()
-                    is Spare -> frame.score + nextRoll()
-                    else -> frame.score
-                }
-            }.sum()
-}
+fun tenPinBowlingScore(rawFrameInput: String): Int =
+    rawFrameInput
+        .frames()
+        .mapIndexed { index, frame ->
+            fun nextRoll() = rawFrameInput.frames().getOrNull(index + 1)?.firstRollScore ?: 0
+            fun nextNextRoll() = rawFrameInput.frames().getOrNull(index + 1)?.secondRollScore
+                ?: rawFrameInput.frames().getOrNull(index + 2)?.firstRollScore ?: 0
 
+            when (frame) {
+                is Strike -> frame.score + nextRoll() + nextNextRoll()
+                is Spare -> frame.score + nextRoll()
+                else -> frame.score
+            }
+        }.sum()
--- app/src/test/kotlin/solution/TenPinBowlingTest.kt
+++ app/src/test/kotlin/solution/TenPinBowlingTest.kt
@@ -6,23 +6,23 @@ import kotlin.test.assertEquals
 class TenPinBowlingTest {
     @Test
     fun `ten pin bowling`() {
-        assertEquals(20, TenPingBowling().score("11 11 11 11 11 11 11 11 11 11"));
-        assertEquals(300, TenPingBowling().score("X X X X X X X X X XXX"))
+        assertEquals(20, tenPinBowlingScore("11 11 11 11 11 11 11 11 11 11"))
+        assertEquals(300, tenPinBowlingScore("X X X X X X X X X XXX"))
     }
 
     @Test
     fun `basic strike`() {
-        assertEquals(10, TenPingBowling().score("X"));
-        assertEquals(28, TenPingBowling().score("X 54"));
-        assertEquals(30, TenPingBowling().score("X X"));
+        assertEquals(10, tenPinBowlingScore("X"))
+        assertEquals(28, tenPinBowlingScore("X 54"))
+        assertEquals(30, tenPinBowlingScore("X X"))
     }
     @Test
     fun `incomplete pins`() {
-        assertEquals(26, TenPingBowling().score("54 72 44"));
+        assertEquals(26, tenPinBowlingScore("54 72 44"))
     }
     @Test
     fun `basic spare`() {
-        assertEquals(24, TenPingBowling().score("9/ 54"));
-        assertEquals(150, TenPingBowling().score("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5"));
+        assertEquals(24, tenPinBowlingScore("9/ 54"))
+        assertEquals(150, tenPinBowlingScore("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5"))
     }
 }
