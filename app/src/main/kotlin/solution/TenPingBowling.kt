026e8d342aba099bb54485c4a9d8d4e1e36ed884
Xuemin Guan <gxuemin@gmail.com>
2023-03-03 09:18:04 +0000

    XG: a bit of refactoring

--- app/src/main/kotlin/solution/TenPingBowling.kt
+++ app/src/main/kotlin/solution/TenPingBowling.kt
@@ -9,17 +9,19 @@ import solution.Frame.Strike
  * https://www.codewars.com/kata/5531abe4855bcc8d1f00004c/train/java
  */
 
-fun tenPinBowlingScore(rawFrameInput: String): Int =
-    rawFrameInput
-        .frames()
+fun tenPinBowlingScore(rawFrameInput: String): Int {
+    val frames = rawFrameInput.frames()
+    
+    return frames
         .mapIndexed { index, frame ->
-            fun nextRoll() = rawFrameInput.frames().getOrNull(index + 1)?.firstRollScore ?: 0
-            fun nextNextRoll() = rawFrameInput.frames().getOrNull(index + 1)?.secondRollScore
-                ?: rawFrameInput.frames().getOrNull(index + 2)?.firstRollScore ?: 0
+            fun nextRollScore() = frames.getOrNull(index + 1)?.firstRollScore ?: 0
+            fun nextNextRollScore() = frames.getOrNull(index + 1)?.secondRollScore
+                ?: frames.getOrNull(index + 2)?.firstRollScore ?: 0
 
             when (frame) {
-                is Strike -> frame.score + nextRoll() + nextNextRoll()
-                is Spare -> frame.score + nextRoll()
+                is Strike -> frame.score + nextRollScore() + nextNextRollScore()
+                is Spare -> frame.score + nextRollScore()
                 else -> frame.score
             }
         }.sum()
+}
